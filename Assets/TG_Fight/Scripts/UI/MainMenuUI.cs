29ab25074b4251a559266c4b533a96f94bcb1457
Intaj <intajcse@gmail.com>
2018-01-14 01:42:30 +0530

    UI update.

--- Assets/TG_Fight/Scripts/UI/MainMenuUI.cs
+++ Assets/TG_Fight/Scripts/UI/MainMenuUI.cs
@@ -19,4 +19,14 @@ public class MainMenuUI : MonoBehaviour
 		uiManager.gamePlayUI.gameObject.SetActive (true);
 		GameManager.instance.OnGameModeSelected (a);
 	}
+
+	public void OnClickWhatsAppShare ()
+	{
+		UIAnimationController.Instance.OnClickShare ();
+	}
+
+	public void OnClickFBShare ()
+	{
+		UIAnimationController.Instance.OnClickShare ();
+	}
 }
---
+++ Assets/TG_Fight/Scripts/UI/UIAnimationController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIAnimationController : MonoBehaviour
+{
+	
+	public static UIAnimationController Instance;
+	public GameObject sharePanel;
+	private bool isShareOn = false;
+
+	void Awake ()
+	{
+		Instance = this;
+	}
+
+	void Start ()
+	{
+		//OnClickShare ();
+	}
+
+	public void OnClickShare ()
+	{
+		isShareOn = !isShareOn;
+		Debug.Log ("OnClickShare");
+		if (isShareOn)
+			LeanTween.scale (sharePanel, Vector3.one, 0.25f).setEaseInSine ();
+		else
+			LeanTween.scale (sharePanel, Vector3.zero, 0.25f).setEaseInExpo ();
+	}
+}
