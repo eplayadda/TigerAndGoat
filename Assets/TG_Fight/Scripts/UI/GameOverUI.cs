187ea4e6208c63e325c1675135c74650e7efaa34
Intaj <intajcse@gmail.com>
2018-02-10 15:02:27 +0530

    Canvas Update.

--- Assets/TG_Fight/Scripts/UI/GameOverUI.cs
+++ Assets/TG_Fight/Scripts/UI/GameOverUI.cs
@@ -3,26 +3,28 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
 
-public class GameOverUI : MonoBehaviour {
+public class GameOverUI : MonoBehaviour
+{
 	UIManager uiManager;
 
 	public Text msgTxt;
 	public Text winStatus;
-	void OnEnable()
+
+	void OnEnable ()
 	{
 		uiManager = UIManager.instance;
 		if (BordManager.instace.currWinStatus == BordManager.eWinStatus.tiger) {
 			GameManager.instance.currGameStatus = eGameStatus.over;	
-			msgTxt.text = "Tiger won Game";
+			msgTxt.text = "Tiger Win The Game";
 		} else {
 			GameManager.instance.currGameStatus = eGameStatus.over;	
-			msgTxt.text = "Goat won Game";
+			msgTxt.text = "Goat Win The Game";
 		}
 		GameResult ();
 
 	}
 
-	void GameResult()
+	void GameResult ()
 	{
 		if (BordManager.instace.currWinStatus.ToString () == GameManager.instance.myAnimalType.ToString ()) {
 			ScoreHandler.instance.SetScore ();
@@ -32,7 +34,8 @@ public class GameOverUI : MonoBehaviour {
 
 		}
 	}
-	public void OnReplay()
+
+	public void OnReplay ()
 	{
 		uiManager.gamePlayUI.gameObject.SetActive (false);
 		uiManager.mainMenuUI.gameObject.SetActive (true);
